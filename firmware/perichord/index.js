bc2f97835d10baa128f032d2668646d33dcebac0
Jordan Mann <jordaloo@gmail.com>
2025-10-17 21:50:53 -0700

    feat: make button a little nicer

--- firmware/perichord/index.js
+++ firmware/perichord/index.js
@@ -3,13 +3,19 @@ powerButton.disabled = true;
 
 const perichord = await (await import("./dist/perichord.js")).default();
 const worklet = new perichord.PerichordAudioWorklet(() => {
-  // Setup the Teensy Audio library components
-  worklet.setup();
-
   powerButton.disabled = false;
-  document.getElementById("power").addEventListener("click", async () => {
-    await worklet.resume();
-  });
 });
 const audioContext = perichord.emscriptenGetAudioObject(worklet.audioContext);
-await audioContext.suspend(); // autoplay is sometimes on in localhost
+// await audioContext.suspend(); // autoplay is sometimes on in localhost
+audioContext.addEventListener("statechange", () => {
+  powerButton.textContent =
+    audioContext.state === "running" ? "Power Off" : "Power On";
+});
+
+powerButton.addEventListener("click", async () => {
+  if (audioContext.state === "running") {
+    await audioContext.suspend();
+  } else {
+    await audioContext.resume();
+  }
+});
